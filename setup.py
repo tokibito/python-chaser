23d9a8747da9cf6aa2dafa82936f09a321ccb9da
Shinya Okano <tokibito@gmail.com>
2013-10-09 21:18:37 +0900

    setup.py追加

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='nullpobug.chaser',
+    version='0.1',
+    description='CHaser 2012 client library for Python',
+    author='Shinya Okano',
+    classifiers=[
+        'Programming Language :: Python :: 2.7',
+    ],
+    packages=find_packages(),
+)
